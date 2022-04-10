8d07e80d6621ac288b40bdc33a251a5d3d183251
vladislav <fetisov>
2022-04-10 18:55:19 +0300

    add Builder

--- src/test/java/core/User.java
+++ src/test/java/core/User.java
@@ -3,11 +3,15 @@ package core;
 import com.google.gson.JsonElement;
 import core.ok.Utils;
 
+import java.util.Date;
+
 public class User {
 
     private final String USER_LOGIN;
     private final String USER_PASSWORD;
     private final String USER_FIO;
+    private String phoneNumber;
+    private Date birthday;
 
     public User(JsonElement jsonElement) {
         this.USER_LOGIN = Utils.removeBrackets(jsonElement.getAsJsonObject().get("login").toString());
@@ -25,5 +29,35 @@ public class User {
 
     public String getFIO() {
         return USER_FIO;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public static class UserBuilder {
+        private final User user;
+
+        public UserBuilder(User user) {
+            this.user = user;
+        }
+
+        public UserBuilder addPhoneNumber(String phoneNumber) {
+            user.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public UserBuilder addBirthday(Date birthday) {
+            user.birthday = birthday;
+            return this;
+        }
+
+        public User build() {
+            return user;
+        }
     }
 }
